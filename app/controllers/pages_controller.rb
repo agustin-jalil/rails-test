7c5a3e5f520cadcafe5e8bcbdd83e1824c788fdc
Agustin Jalil <agustinjalilleon@gmail.com>
2024-01-22 15:43:03 -0300

    rails-test

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+    # Your controller logic goes here
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  # Other routes go here
+end
